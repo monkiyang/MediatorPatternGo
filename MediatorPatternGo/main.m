e572c2a7a01fea51425ba1ee14a0be84a6daacbc
YangMengQi <597337796@qq.com>
2017-05-09 22:45:45 +0800

    初始化提交

---
+++ MediatorPatternGo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MediatorPatternGo
+//
+//  Created by YangMengqi on 2017/5/9.
+//  Copyright © 2017年 MengQi Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
